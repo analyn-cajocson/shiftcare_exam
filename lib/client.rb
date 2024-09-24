3574b100f98d4ec611c76ccf7e8e91bab4857b78
Analyn Cajocson <analyncajocson@gmail.com>
2024-09-24 21:59:17 +0800

    Adds error handling and improves return structure

--- lib/client.rb
+++ lib/client.rb
@@ -7,10 +7,29 @@ class Client
   end
 
   def search(value, field='full_name')
-    @file.select{|record| record[field] == value}
+    return error_response("Invalid search field") unless @file.flat_map{ |item| item.keys }.include?(field)
+
+    result = @file.select{|record| record[field] == value}
+    success_response(result)
   end
 
   def check_duplicates
-    @file.group_by{|record| record['email']}.select { |_key, value| value.size > 1 }.values.flatten
+    result = @file.group_by{|record| record['email']}.select { |_key, value| value.size > 1 }.values.flatten
+    success_response(result)
   end
+
+  private
+    def success_response(data)
+      {
+        "status" => "success",
+        "data" => data
+      }
+    end
+
+    def error_response(msg)
+      {
+        "status" => "failed",
+        "error" => msg
+      }
+    end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,24 +8,31 @@ describe Client do
 
   describe '#search' do
     it 'returns the matched query' do
-      expect(@client.search('Ava Taylor')).to eq([{ "id" => 9,
-                                                    "full_name" => "Ava Taylor",
-                                                    "email" => "ava.taylor@mail.com"
-                                                  }])
+      expect(@client.search('Ava Taylor')).to eq({"status" => "success",
+                                                  "data" => [{ "id" => 9,
+                                                              "full_name" => "Ava Taylor",
+                                                              "email" => "ava.taylor@mail.com"
+                                                            }]
+                                                })
+    end
+
+    it 'returns error if the field is not existing' do
+      expect(@client.search('Ava Taylor', 'surname')).to eq({ "status" => "failed", "error" => "Invalid search field"})
     end
   end
 
   describe '#check_duplicates' do
     it 'returns all the duplicate emails' do
-      expect(@client.check_duplicates).to eq([{"id" => 2,
-                                                "full_name" => "Jane Smith",
-                                                "email" => "jane.smith@yahoo.com"
-                                              },
-                                              {"id" => 15,
-                                                "full_name" => "Another Jane Smith",
-                                                "email" => "jane.smith@yahoo.com"
-                                              }
-                                              ])
+      expect(@client.check_duplicates).to eq({"status" => "success",
+                                              "data" => [{"id" => 2,
+                                                            "full_name" => "Jane Smith",
+                                                            "email" => "jane.smith@yahoo.com"
+                                                          },
+                                                          {"id" => 15,
+                                                            "full_name" => "Another Jane Smith",
+                                                            "email" => "jane.smith@yahoo.com"
+                                                          }]
+                                              })
     end
   end
 end
